199c434395649fc9df7b9f35826682a9e834b306
Rafał Kot <rafalkot@gmail.com>
2018-02-14 19:10:02 +0100

    init

---
+++ app/src/main/java/pl/onet/sympatia/payments/MainActivity.java
@@ -0,0 +1,40 @@
+package pl.onet.sympatia.payments;
+
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //must be called before setContentView(...)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToPayments (View view) {
+        goToUrl ( "https://payments.sympatia.onet.pl/android.html");
+    }
+
+    private void goToUrl (String url) {
+        Intent intent = new Intent(this, WebViewActivity.class);
+        intent.putExtra("url", url);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/pl/onet/sympatia/payments/WebViewActivity.java
@@ -0,0 +1,46 @@
+package pl.onet.sympatia.payments;
+
+/**
+ * Created by Rafał on 2018-02-14.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends Activity {
+
+    private WebView webView;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+        Intent i = getIntent();
+        String url= i.getStringExtra("url");
+        webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        CookieSyncManager.createInstance(getApplicationContext());
+        CookieSyncManager.getInstance().startSync();
+        CookieManager.getInstance().setAcceptCookie(true);
+
+        String cookieString = "access_token=aaaa;";
+
+        CookieManager c = CookieManager.getInstance();
+        Log.v("Build: ",String.valueOf(Build.VERSION.SDK_INT));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            c.setCookie("https://payments.sympatia.onet.pl", cookieString);
+            Log.v("Cookies: ", c.getCookie("https://payments.sympatia.onet.pl"));
+        }
+        webView.loadUrl(url);
+
+    }
+
+}
